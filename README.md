2e15bd6fa784c842335b5cfd4970d94db11426ef
Michel Verbraak <info@1st-setup.nl>
2013-06-18 21:23:57 +0200

    Finished version 1.0.0 It can find NZB files at your usenet indexer website. It will show the results and a button to automatically send the NZB file to SABnzbd.
    Signed-off-by: Michel Verbraak <info@1st-setup.nl>

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ easynzbdl
 =========
 
 Add-on for easy nzb download
+
+Install the add-on in Firefox. Got Firefox's preference dialog and enter the required details for search provider and download provider.
+Goto a movie in www.imdb.com and it will start a search for the NZB files at the usenet indexer of your choice.
+Once it found some NZB files you get the option to send the NZB file directly to SABnzbd throug the REST api interface of SABnzbd.
+Keep an eye on the bottom of your firefox window.
+
--- chrome/content/browser.js
+++ chrome/content/browser.js
@@ -24,54 +24,206 @@ var Cc = Components.classes;
 Cu.import("resource://easynzbdl/globalFunctions.js");
 Cu.import("resource://interfaces/xml2jxon/mivIxml2jxon.js");
 
-function easynzbdlGetNZB(aImdbId, aDocument, searchResultBox)
+function easynzbdlDownloadNZB(aImdbId, aDocument, aWindow, searchResultBox, aItem)
 {
-	this.req = Cc["@mozilla.org/xmlextras/xmlhttprequest;1"].createInstance();
-
 	this.imdbId = aImdbId;
 	this._document = aDocument;
+	this._window = aWindow;
+	this.searchResultBox = searchResultBox;
+	this.item = aItem;
+}
+
+easynzbdlDownloadNZB.prototype = {
+
+	startDownload: function _startDownload(aElement)
+	{
+		dump("easynzbdlDownloadNZB.startDownload\n");
+		this._element = aElement;
+
+		let serverurl = enzbdlSafeGetCharPref(null, "extensions.1st-setup.easynzbdl.download.serverurl", "", true);
+		if (serverurl[serverurl.length-1] != "/") {
+			serverurl = serverurl + "/";
+		}
+		let apikey = encodeURIComponent(enzbdlSafeGetCharPref(null, "extensions.1st-setup.easynzbdl.download.apikey", "", true));
+
+		if ((serverurl == "/") || (apikey == "")) {
+			dump("  Not all preferences are available. Going to open pref dialog.\n");
+			alert("Not all preferences for download provider are available. Going to open pref dialog.");
+			this._window.openDialog("chrome://browser/content/preferences/preferences.xul",
+				"Preferences",
+				"chrome,titlebar,toolbar,centerscreen,dialog,modal=yes,resizable=no",
+				null); 
+
+			serverurl = enzbdlSafeGetCharPref(null, "extensions.1st-setup.easynzbdl.download.serverurl", "", true);
+			if (serverurl[serverurl.length-1] != "/") {
+				serverurl = serverurl + "/";
+			}
+			apikey = encodeURIComponent(enzbdlSafeGetCharPref(null, "extensions.1st-setup.easynzbdl.download.apikey", "", true));
+
+			if ((serverurl == "/") || (apikey == "")) {
+				alert("You did not enter all preferences for download provider! Aborting download.");
+				this._element.setAttribute("status", "(Aborted sending NZB to SABnzbd)");
+				return;
+			}
+		}
+
+		// Get categories from SABnzbd
+		this.req = Cc["@mozilla.org/xmlextras/xmlhttprequest;1"].createInstance();
+
+		var self = this;
+
+		// http://dvcs.w3.org/hg/progress/raw-file/tip/Overview.html
+		// https://developer.mozilla.org/en/XPCOM_Interface_Reference/nsIXMLHttpRequestEventTarget
+		this.req.addEventListener("loadend", function(evt) { self.loadend(evt); }, false);
+
+		//dump("easynzbdlGetNZB.new: Going to get: "+serverurl+"api?t=movie&imdbid="+finaleImdbId+"&apikey="+apikey+"&extended=1\n");
+	 
+		this.req.open("GET", serverurl+"api?mode=get_cats&output=json&apikey="+apikey, true);
+		this.req.send();
+		dump("easynzbdlDownloadNZB.startDownload: send GET request.\n");
+		this._element.setAttribute("status", "(Getting categories from SABnzbd)");
+	},
+
+	loadend: function _loadend(event)
+	{
+		dump("easynzbdlDownloadNZB.loadend\n");
+		let req = this.req;
+
+		if (this.debug) this.logInfo(": easynzbdlDownloadNZB.loadend :"+event.type+", readyState:"+req.readyState+", status:"+req.status);
+		if (this.debug) this.logInfo(": easynzbdlDownloadNZB.loadend :"+req.responseText,2);
+
+		//this.exchangeStatistics.addDataRead(this.currentUrl, xmlReq.responseText.length);
+
+		if (req.readyState != 4) {
+			dump("readyState < 4. THIS SHOULD NEVER HAPPEN. PLEASE REPORT.");
+			return;
+		}
+
+		this._element.setAttribute("status", "(Received categories from SABnzbd)");
+
+		dump("easynzbdlDownloadNZB.loadend: "+req.responseText+"\n");
+		var answer = JSON.parse(req.responseText);
+		// Let user choose category. Is for later.
+
+		// Get categories from SABnzbd
+		this.req2 = Cc["@mozilla.org/xmlextras/xmlhttprequest;1"].createInstance();
+
+		var self = this;
+
+		// http://dvcs.w3.org/hg/progress/raw-file/tip/Overview.html
+		// https://developer.mozilla.org/en/XPCOM_Interface_Reference/nsIXMLHttpRequestEventTarget
+		this.req2.addEventListener("loadend", function(evt) { self.loadendSendToSABnzbd(evt); }, false);
+
+		let serverurl = enzbdlSafeGetCharPref(null, "extensions.1st-setup.easynzbdl.download.serverurl", "", true);
+		if (serverurl[serverurl.length-1] != "/") {
+			serverurl = serverurl + "/";
+		}
+		let apikey = encodeURIComponent(enzbdlSafeGetCharPref(null, "extensions.1st-setup.easynzbdl.download.apikey", "", true));
+
+		var nzbUrl = encodeURIComponent(this.item.getTagValue("_default_:link", ""));
+		var niceName = encodeURIComponent(this.item.getTagValue("_default_:title", ""));
+
+		dump("easynzbdlGetNZB.new: Going to get: "+serverurl+"api?mode=addurl&name="+nzbUrl+"&nzbname="+niceName+"&apikey="+apikey+"\n");
+
+		this.req2.open("GET", serverurl+"api?mode=addurl&name="+nzbUrl+"&nzbname="+niceName+"&apikey="+apikey, true);
+		this.req2.send();
+		//dump("easynzbdlDownloadNZB.loadend: send GET request.\n");
+		this._element.setAttribute("status", "(Sending NZB to SABnzbd)");
+
+	},
+
+	loadendSendToSABnzbd: function _loadendSendToSABnzbd(event)
+	{
+		//dump("easynzbdlDownloadNZB.loadendSendToSABnzbd\n");
+		let req = this.req2;
+
+		if (this.debug) this.logInfo(": easynzbdlDownloadNZB.loadendSendToSABnzbd :"+event.type+", readyState:"+req.readyState+", status:"+req.status);
+		if (this.debug) this.logInfo(": easynzbdlDownloadNZB.loadendSendToSABnzbd :"+req.responseText,2);
+
+		//this.exchangeStatistics.addDataRead(this.currentUrl, xmlReq.responseText.length);
+
+		if (req.readyState != 4) {
+			//dump("readyState < 4. THIS SHOULD NEVER HAPPEN. PLEASE REPORT.");
+			return;
+		}
+
+		this._element.setAttribute("status", "(Sent NZB to SABnzbd)");
+
+		//dump("easynzbdlDownloadNZB.loadendSendToSABnzbd: "+req.responseText+"\n");
+		//var answer = JSON.parse(req.responseText);
+	},
+}
+
+function easynzbdlGetNZB(aImdbId, aDocument, aWindow, searchResultBox)
+{
+	this.imdbId = aImdbId;
+	this._document = aDocument;
+	this._window = aWindow;
 	this.searchResultBox = searchResultBox;
 
-	this.items = [];
+	let serverurl = enzbdlSafeGetCharPref(null, "extensions.1st-setup.easynzbdl.search.serverurl", "", true);
+	if (serverurl[serverurl.length-1] != "/") {
+		serverurl = serverurl + "/";
+	}
+	let apikey = encodeURIComponent(enzbdlSafeGetCharPref(null, "extensions.1st-setup.easynzbdl.search.apikey", "", true));
 
-	var tmp = this;
+	if ((serverurl == "/") || (apikey == "")) {
+		//dump("  Not all preferences are available. Going to open pref dialog.\n");
+		alert("Not all preferences for search provider are available. Going to open pref dialog.");
+		this._window.openDialog("chrome://browser/content/preferences/preferences.xul",
+			"Preferences",
+			"chrome,titlebar,toolbar,centerscreen,dialog,modal=yes,resizable=no",
+			null); 
+
+		serverurl = enzbdlSafeGetCharPref(null, "extensions.1st-setup.easynzbdl.search.serverurl", "", true);
+		if (serverurl[serverurl.length-1] != "/") {
+			serverurl = serverurl + "/";
+		}
+		apikey = encodeURIComponent(enzbdlSafeGetCharPref(null, "extensions.1st-setup.easynzbdl.search.apikey", "", true));
+
+		if ((serverurl == "/") || (apikey == "")) {
+			alert("You did not enter all preferences for search provider! Aborting search.");
+			this._element.setAttribute("status", "(Aborted search for NZB files.)");
+			return;
+		}
+	}
+
+
+	this.req = Cc["@mozilla.org/xmlextras/xmlhttprequest;1"].createInstance();
+
+	var self = this;
 
 	// http://dvcs.w3.org/hg/progress/raw-file/tip/Overview.html
 	// https://developer.mozilla.org/en/XPCOM_Interface_Reference/nsIXMLHttpRequestEventTarget
-	this.req.addEventListener("loadend", function(evt) { tmp.loadend(evt); }, false);
+	this.req.addEventListener("loadend", function(evt) { self.loadend(evt); }, false);
 
 	let finaleImdbId = aImdbId;
 	if (finaleImdbId.indexOf("tt") == 0) {
 		finaleImdbId = finaleImdbId.substr(2);
 	}
 
-	let serverurl = enzbdlSafeGetCharPref(null, "extensions.1st-setup.easynzbdl.search.serverurl", "", true);
-	if (serverurl[serverurl.length-1] != "/") {
-		serverurl = serverurl + "/";
-	}
-	let apikey = enzbdlSafeGetCharPref(null, "extensions.1st-setup.easynzbdl.search.apikey", "", true);
-
 	//dump("easynzbdlGetNZB.new: Going to get: "+serverurl+"api?t=movie&imdbid="+finaleImdbId+"&apikey="+apikey+"&extended=1\n");
  
 	this.req.open("GET", serverurl+"api?t=movie&imdbid="+finaleImdbId+"&apikey="+apikey+"&extended=1", true);
 	this.req.send();
 	dump("easynzbdlGetNZB.new: send GET request.\n");
+	this.searchResultBox.setAttribute("status", "(Searching)");
 }
 
 easynzbdlGetNZB.prototype = {
 
 	loadend: function _loadend(event)
 	{
-		dump("easynzbdlGetNZB.loadend\n");
+		//dump("easynzbdlGetNZB.loadend\n");
 		let req = this.req;
 
-		if (this.debug) this.logInfo(": ExchangeRequest.loadend :"+event.type+", readyState:"+req.readyState+", status:"+req.status);
-		if (this.debug) this.logInfo(": ExchangeRequest.loadend :"+req.responseText,2);
+		if (this.debug) this.logInfo(": easynzbdlGetNZB.loadend :"+event.type+", readyState:"+req.readyState+", status:"+req.status);
+		if (this.debug) this.logInfo(": easynzbdlGetNZB.loadend :"+req.responseText,2);
 
 		//this.exchangeStatistics.addDataRead(this.currentUrl, xmlReq.responseText.length);
 
 		if (req.readyState != 4) {
-			dump("readyState < 4. THIS SHOULD NEVER HAPPEN. PLEASE REPORT.");
+			//dump("readyState < 4. THIS SHOULD NEVER HAPPEN. PLEASE REPORT.");
 			return;
 		}
 
@@ -88,19 +240,21 @@ easynzbdlGetNZB.prototype = {
 		//dump("newXML:\n"+newXML.toString()+"\n");
 
 		var items = newXML.XPath("/_default_:rss/_default_:channel/_default_:item");
-		dump("items.length:"+items.length+"\n");
+		//dump("items.length:"+items.length+"\n");
 		if (items.length > 0) {
 
 			var vbox = this._document.getElementById("easynzbdl-appcontent-vbox");
 
 			for (var i=0; i<items.length; i++) {
-				dump("item Title:"+items[i].getTagValue("_default_:title")+"\n");
-				var searchResultItem = this._document.createElementNS("http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul", "xul:easynzbdl-searchResultItem");
-				this.searchResultBox.addResultItem(searchResultItem);
+				//dump("item Title:"+items[i].getTagValue("_default_:title")+"\n");
+				var nzbItem = new easynzbdlDownloadNZB(this.imdbId, this._document, this._window, this.searchResultBox, items[i]);
+				this.searchResultBox.addResultItem(items[i], nzbItem);
 			}
 		}
-		dump("easynzbdlGetNZB.loadend DONE\n");
+		//dump("easynzbdlGetNZB.loadend DONE\n");
+		this.searchResultBox.setAttribute("status", "("+items.length+" results)");
 	},
+
 }
 
 function easynzbdlBrowser(aDocument, aWindow)
@@ -116,7 +270,7 @@ easynzbdlBrowser.prototype = {
 
 	onLoad: function _onLoad()
 	{
-		dump("easynzbdlBrowser.ondLoad\n");
+		//dump("easynzbdlBrowser.ondLoad\n");
 
 		if (typeof getBrowser == 'function') {
 			var self = this
@@ -140,14 +294,14 @@ easynzbdlBrowser.prototype = {
 
 	parsePage: function _parsePage(event)
 	{
-		dump("easynzbdlBrowser.parsePage\n");
+		//dump("easynzbdlBrowser.parsePage\n");
 		if (!event) return;
 
 		if (event.target.body == null) return;
 		if (event.eventPhase != 1) return;
 
 		if (event.target["location"]) {
-			dump("easynzbdlBrowser.parsePage: location:"+event.target.location+", event.eventPhase:"+event.eventPhase+"\n");
+			//dump("easynzbdlBrowser.parsePage: location:"+event.target.location+", event.eventPhase:"+event.eventPhase+"\n");
 
 			let imdbId = this.getImdbId(event.target.location.toString());
 			if (imdbId) {
@@ -156,6 +310,7 @@ easynzbdlBrowser.prototype = {
 						this._markedPages[this._lastImdbId].searchResultBox.hide();
 					}
 					this._markedPages[imdbId].searchResultBox.show();
+					this._lastImdbId = imdbId;
 					return;
 				}
 
@@ -164,11 +319,16 @@ easynzbdlBrowser.prototype = {
 				searchResultBox.id = imdbId;
 				let appcontent = this._document.getElementById("appcontent");
 				appcontent.appendChild(searchResultBox);
-				
-				this._markedPages[imdbId] = { searchResultBox: searchResultBox,
-								searchObject: new easynzbdlGetNZB(imdbId, this._document, searchResultBox)};
+				this._markedPages[imdbId] = { searchResultBox: searchResultBox };
+				this._markedPages[imdbId]["searchObject"] = new easynzbdlGetNZB(imdbId, this._document, this._window, searchResultBox);
 				this._lastImdbId = imdbId;
-				dump("easynzbdlBrowser.parsePage: imdbId:"+imdbId+"\n");
+				//dump("easynzbdlBrowser.parsePage: imdbId:"+imdbId+"\n");
+			}
+			else {
+				if ((this._lastImdbId) && (this._markedPages[this._lastImdbId])) {
+					this._markedPages[this._lastImdbId].searchResultBox.hide();
+					this._lastImdbId = null;
+				}
 			}
 		}
 	},
@@ -176,10 +336,10 @@ easynzbdlBrowser.prototype = {
 	getBrowserForTab: function _getBrowserForTab(aTab)
 	{
 		if (gBrowser) {
-			dump("easynzbdlBrowser.getBrowserForTab: we have a gBrowser.\n");
+			//dump("easynzbdlBrowser.getBrowserForTab: we have a gBrowser.\n");
 			let tabBrowser = gBrowser.getBrowserForTab(aTab);
 			if (tabBrowser) {
-				dump("easynzbdlBrowser.getBrowserForTab: we have a tabBrowser.\n");
+				//dump("easynzbdlBrowser.getBrowserForTab: we have a tabBrowser.\n");
 				return tabBrowser;
 			}
 		}
@@ -189,47 +349,52 @@ easynzbdlBrowser.prototype = {
 
 	onTabSelect: function _onTabSelect(event)
 	{
-		dump("easynzbdlBrowser.onTabSelect\n");
+		//dump("easynzbdlBrowser.onTabSelect\n");
 
 		let tabBrowser = this.getBrowserForTab(event.target);
 
 		if (tabBrowser) {
-			dump("easynzbdlBrowser.onTabSelect: tabBrowser.currentURI:"+tabBrowser.currentURI.spec+"\n");
+			//dump("easynzbdlBrowser.onTabSelect: tabBrowser.currentURI:"+tabBrowser.currentURI.spec+"\n");
 
 			let imdbId = this.getImdbId(tabBrowser.currentURI.spec);
+
+			if ((this._lastImdbId) && (this._markedPages[this._lastImdbId])) {
+				this._markedPages[this._lastImdbId].searchResultBox.hide();
+				//dump("easynzbdlBrowser.onTabSelect: hidden label. this._lastImdbId:"+this._lastImdbId+"\n");
+			}
+
 			if (imdbId) {
 				if (this._markedPages[imdbId]) {
 					this._markedPages[imdbId].searchResultBox.show();
+					//dump("easynzbdlBrowser.onTabSelect: shown label. this._lastImdbId:"+imdbId+"\n");
+					this._lastImdbId = imdbId;
 				}
-				dump("easynzbdlBrowser.onTabSelect: imdbId:"+imdbId+"\n");
-			}
-			else {
-				if ((this._lastImdbId) && (this._markedPages[this._lastImdbId])) {
-					this._markedPages[this._lastImdbId].searchResultBox.hide();
-				}
-				dump("easynzbdlBrowser.parsePage: hidden label.\n");
+				//dump("easynzbdlBrowser.onTabSelect: imdbId:"+imdbId+"\n");
 			}
 		}
 	},
 
 	onTabClose: function _onTabClose(event)
 	{
-		dump("easynzbdlBrowser.onTabClose\n");
+		//dump("easynzbdlBrowser.onTabClose\n");
 		let tabBrowser = this.getBrowserForTab(event.target);
 
 		let tabBrowser = this.getBrowserForTab(event.target);
 
 		if (tabBrowser) {
-			dump("easynzbdlBrowser.onTabClose: tabBrowser.currentURI:"+tabBrowser.currentURI.spec+"\n");
+			//dump("easynzbdlBrowser.onTabClose: tabBrowser.currentURI:"+tabBrowser.currentURI.spec+"\n");
 
 			let imdbId = this.getImdbId(tabBrowser.currentURI.spec);
 			if (imdbId) {
-				dump("easynzbdlBrowser.onTabClose: imdbId:"+imdbId+"\n");
+				//dump("easynzbdlBrowser.onTabClose: imdbId:"+imdbId+"\n");
 				if (this._markedPages[imdbId]) {
 					this._markedPages[imdbId].searchResultBox.hide();
 				}
 				this._markedPages[imdbId] = null;
 				delete this._markedPages[imdbId];
+				if (imdbId == this._lastImdbId) {
+					this._lastImdbId = null;
+				}
 			}
 		}
 
